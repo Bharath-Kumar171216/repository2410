7bd104b5a9e4b5faf9f73e16165a81e753486059
Veeravalli Bharath Kumar <bharathveeravalli786@gmail.com>
2025-02-22 17:18:49 +0530

    added some information in local and remote repository

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>This heading is from react application.</h1>
     <h2>This changes done through remote repository</h2>
+    <h3>This changes done through local repository</h3>
       <p>my self bharath</p>
     </div>
   );
