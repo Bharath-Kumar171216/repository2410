561d6154d512b4b1d577aa729861d5e19941bb3f
Bharath Kumar <bharathveeravalli786@gmail.com>
2025-02-22 17:16:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This heading is from react application.</h1>
+    <h2>This changes done through remote repository</h2>
       <p>my self bharath</p>
     </div>
   );
