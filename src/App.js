e6fce848ae7d87544faf6e96b48f8be572328f6c
Veeravalli Bharath Kumar <bharathveeravalli786@gmail.com>
2025-02-22 17:06:15 +0530

    added intial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This heading is from react application.</h1>
+      <p>my self bharath</p>
+    </div>
+  );
+}
+
+export default App;
